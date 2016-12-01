34e08139a7a9c82ecacff014bf454a22c8ec16e9
Glenn Yonemitsu <yonemitsu@gmail.com>
2016-12-01 01:08:12 -0500

    some magic methods

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
-from zizu import Tag
+from zizu import Tag, Template
 from util import render
 
 
-h = Tag('html')
+t = Template('root')
+h = t.html
 h.p(foo='bar')
 h += 'hi there'
+h += Template('test')
 
 table = h.table(id='branched off')
 
 for i in range(2):
     tr = table.tr
-    for j in range(4):
+    for j in range(2):
         tr.td(Tag('something' + str(j)))
 
 
+t['test'] = 'yes, hi indeed.'
 print(render(h, True))
 
--- zizu.py
+++ zizu.py
@@ -1,15 +1,54 @@
 class Node:
 
     def __init__(self):
-        self._children = []
+        self.__root = self
+        self.__children = []
 
     def __iadd__(self, *nodes):
-        nodes = self._wash_nodes(*nodes)
-        self._children.extend(nodes)
+        self._append(*nodes)
         return self
 
+    def _wash_nodes_hook(self, *nodes):
+        return list(self._set_root_for_nodes(*self._wash_nodes(*nodes)))
+
+    def _set_root_for_nodes(self, *nodes):
+        for node in nodes:
+            node.__root = self._root
+            yield node
+
     def _wash_nodes(self, *nodes):
-        return nodes
+        for node in nodes:
+            yield node
+
+    def _append(self, *nodes):
+        self.__children += list(self._wash_nodes_hook(*nodes))
+        
+    def _prepend(self, *nodes):
+        self.__children = list(self._wash_nodes_hook(*nodes)) + self.__children
+
+    @property
+    def _children(self):
+        for child in self.__children:
+            yield child
+
+    @property
+    def _root(self):
+        if self.__root is self:
+            return self
+        return self.__root._root
+
+
+class Renderable(Node):
+
+    def __getattr__(self, key):
+        n = Tag(key)
+        self._append(n)
+        return n
+
+    def __call__(self, *args, **kwargs):
+        self._append(*args)
+        self._attrs = kwargs
+        return self
 
     @property
     def _opener(self):
@@ -19,9 +58,6 @@ class Node:
     def _closer(self):
         return ''
 
-
-class Html(Node):
-
     def _wash_nodes(self, *nodes):
         for node in nodes:
             if type(node) is str:
@@ -30,22 +66,35 @@ class Html(Node):
                 yield node
 
 
-class Tag(Html):
+class Template(Renderable):
+
+    def __init__(self, name=None):
+        self._name = name
+        self._content = {}
+        super().__init__()
+
+    def __setitem__(self, name, content):
+        t = Template(None)
+        t += content
+        self._root._content[name] = t
+
+    @property
+    def _children(self):
+        if self._name in self._root._content:
+            for node in self._root._content[self._name]._children:
+                yield node
+        else:
+            # default
+            for child in self.__children:
+                yield child
+
+
+class Tag(Renderable):
 
     def __init__(self, tag=None):
         self._tag = tag
         self._attrs = {}
         super().__init__()
-
-    def __getattr__(self, key):
-        n = Tag(key)
-        self._children.append(n)
-        return n
-
-    def __call__(self, *args, **kwargs):
-        self._children = list(args)
-        self._attrs = kwargs
-        return self
 
     @property
     def _opener(self):
@@ -55,7 +104,8 @@ class Tag(Html):
         if not self._attrs:
             return '<{tag}>'.format(tag=self._tag)
 
-        attrs = ' '.join('{key}="{value}"'.format(key=key, value=value)
+        attrs = ' '.join(
+            '{key}="{value}"'.format(key=key, value=value)
             for key, value in self._attrs.items())
         return '<{tag} {attrs}>'.format(tag=self._tag, attrs=attrs)
 
@@ -66,7 +116,7 @@ class Tag(Html):
         return '</{tag}>'.format(tag=self._tag)
 
 
-class Text(Html):
+class Text(Renderable):
 
     def __init__(self, content=''):
         self._content = content
