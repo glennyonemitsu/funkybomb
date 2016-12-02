ca4e149b1366e8867ba076ffe30283f64346c0a8
Glenn Yonemitsu <yonemitsu@gmail.com>
2016-12-01 23:17:42 -0500

    some unit test for basic node classes

--- tests/node/basic.py
+++ tests/node/basic.py
@@ -1,13 +1,35 @@
 from funkybomb.node import Node
 
 
-def test_thing():
-    n = Node()
-    pass
-
-
-def test_thing2():
+def test_adding_children():
+    root = Node()
+    assert len(root._children) == 0
 
     n = Node()
+    root += n
+    assert len(root._children) == 1
 
-    pass
+    n += Node()
+    assert len(root._children) == 1
+
+    root._append(Node())
+    assert len(root._children) == 2
+
+
+def test_root():
+    root = Node()
+    n = Node()
+    root += n
+    assert n._root == root
+
+    n._append(Node(), Node(), Node())
+    for child in n._children:
+        assert child._root == root
+
+
+def test_children_iteration():
+    children = [Node() for i in range(5)]
+    root = Node()
+    root._append(*children)
+    for i, node in enumerate(root):
+        assert node is children[i]
