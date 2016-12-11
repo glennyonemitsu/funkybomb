aff5ce10cf41a88b1017b63654117a0c0818135b
Glenn Yonemitsu <yonemitsu@gmail.com>
2016-12-11 18:36:04 -0500

    render suffix removed

--- funkybomb/util.py
+++ funkybomb/util.py
@@ -1,25 +1,24 @@
-def generate(node, prefix, indent, suffix):
+def generate(node, prefix, indent):
     if node._opener:
-        yield prefix
-        yield node._opener
-        yield suffix
+        yield prefix + node._opener
 
     new_prefix = prefix
     if node._opener and node._closer:
         new_prefix += indent
 
     for child in node:
-        for output in generate(child, new_prefix, indent, suffix):
+        for output in generate(child, new_prefix, indent):
             yield output
 
     if node._closer:
-        yield prefix
-        yield node._closer
-        yield suffix
+        yield prefix + node._closer
 
 
 def render(node, pretty=False):
-    return ''.join(generate(node, '', '    ', '\n'))
+    if pretty:
+        return '\n'.join(generate(node, '', '    '))
+    else:
+        return ''.join(generate(node, '', ''))
 
 
 def freeze(node):
