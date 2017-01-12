999d272bbfae57966028883a95bfa292f412f464
Glenn Yonemitsu <yonemitsu@gmail.com>
2017-01-11 21:14:26 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Funky Bomb',
-    version='0.2',
+    version='0.2.1',
     description='Quick and wild HTML tree growth',
     author='Glenn Yonemitsu',
     license='Apache License 2.0',
