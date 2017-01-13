5063cc948532a465a3af49515cf9c68fd7e933dd
Glenn Yonemitsu <yonemitsu@gmail.com>
2017-01-12 23:16:17 -0500

    removed examples source files

--- website/examples/use_native_python.py
+++
@@ -1,9 +0,0 @@
-from funkybomb import Template
-from models import users
-
-tmpl = Template()
-table = tmpl.table
-for user in users.get_all():
-    row = table.tr
-    row.td + user.name
-    row.td + user.age
