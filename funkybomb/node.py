d17db49ac98c33379a3a440f76ac7fdcd3f157f2
Glenn Yonemitsu <yonemitsu@gmail.com>
2017-01-21 00:11:25 -0500

    node attribute refactor

--- funkybomb/node.py
+++ funkybomb/node.py
@@ -11,8 +11,18 @@ class Node:
         Initialize the Node.
         """
 
-        self._root_node = self
-        self._children = []
+        self._children_ = []
+
+    def __getattr__(self, key):
+        if key in self.__dict__:
+            return self.__dict__[key]
+        return None
+
+    def __setattr__(self, key, value):
+        if key.startswith('_') and key.endswith('_'):
+            self.__dict__[key] = value
+            return
+        super().__setattr__(key, value)
 
     def __repr__(self):
         """
@@ -22,7 +32,7 @@ class Node:
         return '<BaseNode>'
 
     def __iter__(self):
-        return iter(self._children)
+        return iter(self._children_)
 
     def __add__(self, *nodes):
         """
@@ -32,36 +42,17 @@ class Node:
         # supports: self + ('foo', 'bar', Tag('em') + 'baz') notation
         if len(nodes) == 1 and type(nodes[0]) == tuple:
             nodes = nodes[0]
-        self._append(*nodes)
+        self._append_(*nodes)
         return self
 
-    def _wash_nodes_hook(self, *nodes):
-        return self._set_root_for_nodes(*self._wash_nodes(*nodes))
-
-    def _set_root_for_nodes(self, *nodes):
-        for node in nodes:
-            node._root_node = self._root
-            yield node
-
-    def _wash_nodes(self, *nodes):
+    def _wash_nodes_(self, *nodes):
         for node in nodes:
             yield node
 
-    def _append(self, *nodes):
+    def _append_(self, *nodes):
         if len(nodes) == 1 and type(nodes[0]) is list:
             nodes = nodes[0]
-        self._children.extend(list(self._wash_nodes_hook(*nodes)))
-
-    def _prepend(self, *nodes):
-        if len(nodes) == 1 and type(nodes[0]) is list:
-            nodes = nodes[0]
-        self._children = list(self._wash_nodes_hook(*nodes)) + self._children
-
-    @property
-    def _root(self):
-        if self._root_node is self:
-            return self
-        return self._root_node._root
+        self._children_.extend(list(self._wash_nodes_(*nodes)))
 
 
 class Renderable(Node):
@@ -69,47 +60,41 @@ class Renderable(Node):
     def __repr__(self):
         return '<RenderableNode>'
 
-    def __init__(self, *args, **attrs):
+    def __init__(self, **attrs):
         super().__init__()
-        self._attrs = attrs
+        self._attrs_ = attrs
+        self._opener_ = ''
+        self._closer_ = ''
 
-    def __setattr__(self, key, value):
-        if key.startswith('_'):
+    def __setattrz__(self, key, value):
+        if value is None:
+            return
+
+        if key.startswith('_') and key.endswith('_'):
             super().__setattr__(key, value)
-        else:
-            self._append(value)
+            return
+
+        self._append_(value)
 
     def __getattr__(self, key):
 
         # required for a bunch of magic methods quirks like with deepcopy()
-        if key.startswith('__'):
+        if key.startswith('_') and key.endswith('_'):
             return super().__getattr__(key)
 
         if hide_attribute(key):
             return None
 
-        if key.startswith('_'):
-            setattr(self, key, None)
-            return super().__getattr__(key)
-
         n = Tag(key)
-        self._append(n)
+        self._append_(n)
         return n
 
     def __call__(self, *args, **kwargs):
-        self._append(*args)
-        self._attrs.update(kwargs)
+        self._append_(*args)
+        self._attrs_.update(kwargs)
         return self
 
-    @property
-    def _opener(self):
-        return ''
-
-    @property
-    def _closer(self):
-        return ''
-
-    def _wash_nodes(self, *nodes):
+    def _wash_nodes_(self, *nodes):
         text_types = {str, int, float}
         for node in nodes:
             if type(node) in text_types:
@@ -122,11 +107,11 @@ class Template(Renderable):
 
     def __init__(self, name=None):
         super().__init__()
-        self._name = name
+        self._name_ = name
 
     def __repr__(self):
-        if self._name:
-            return '<TemplateNode[{name}]>'.format(name=self._name)
+        if self._name_:
+            return '<TemplateNode[{name}]>'.format(name=self._name_)
         else:
             return '<AnonymousTemplateNode>'
 
@@ -138,24 +123,41 @@ class Tag(Renderable):
 
     # html 5 tag categories according to
     # https://www.w3.org/TR/html5/syntax.html#void-elements
-    _void_tags = {
+    _void_tags_ = {
         'area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen',
         'link', 'meta', 'param', 'source', 'track', 'wbr'
     }
 
-    _raw_text_tags = {'script', 'style'}
+    _raw_text_tags_ = {'script', 'style'}
 
     def __init__(self, tag=None, **attrs):
         super().__init__(**attrs)
-        self._tag = tag
+        self._tag_ = tag
+        self._void_ = self._tag_ in self._void_tags_
+        self._raw_text_ = self._tag_ in self._raw_text_tags_
+        self._repr_ = '<TagNode[{tag}]>'.format(tag=tag)
+
+        if self._tag_ is None:
+            self._opener_ = ''
+        elif not self._attrs_:
+            self._opener_ = '<{tag}>'.format(tag=self._tag_)
+        else:
+            attrs = build_attrs(self._attrs_)
+            self._opener_ = '<{tag} {attrs}>'.format(
+                tag=self._tag_, attrs=attrs)
+
+        if self._tag_ is None or self._void_:
+            self._closer_ = ''
+        else:
+            self._closer_ = '</{tag}>'.format(tag=self._tag_)
 
     def __repr__(self):
-        return '<TagNode[{tag}]>'.format(tag=self._tag)
+        return self._repr_
 
-    def _wash_nodes(self, *nodes):
-        if self._void and nodes:
+    def _wash_nodes_(self, *nodes):
+        if self._void_ and nodes:
             raise ChildNodeError()
-        elif self._raw_text:
+        elif self._raw_text_:
             text_types = {str, int, float}
             for node in nodes:
                 if type(node) in text_types:
@@ -165,47 +167,19 @@ class Tag(Renderable):
                 else:
                     raise ChildNodeError()
         else:
-            for node in super()._wash_nodes(*nodes):
+            for node in super()._wash_nodes_(*nodes):
                 yield node
-
-    @property
-    def _void(self):
-        return self._tag in self._void_tags
-
-    @property
-    def _raw_text(self):
-        return self._tag in self._raw_text_tags
-
-    @property
-    def _opener(self):
-        if self._tag is None:
-            return ''
-
-        if not self._attrs:
-            return '<{tag}>'.format(tag=self._tag)
-
-        attrs = build_attrs(self._attrs)
-        return '<{tag} {attrs}>'.format(tag=self._tag, attrs=attrs)
-
-    @property
-    def _closer(self):
-        if self._tag is None or self._void:
-            return ''
-        return '</{tag}>'.format(tag=self._tag)
 
 
 class Text(Renderable):
 
     def __init__(self, content=''):
         super().__init__()
-        self._content = str(content)
+        self._content_ = str(content)
+        self._opener_ = str(content)
 
     def __repr__(self):
-        return '<TextNode["{text}"]>'.format(text=self._content)
-
-    @property
-    def _opener(self):
-        return self._content
+        return '<TextNode["{text}"]>'.format(text=self._content_)
 
 
 def build_attrs(attrs):
@@ -240,8 +214,8 @@ def is_ipython():
 
 def bind_template(root, name, node):
     for i, child in enumerate(root):
-        if isinstance(child, Template) and child._name == name:
-            root._children[i] = node
+        if isinstance(child, Template) and child._name_ == name:
+            root._children_[i] = node
             return
         else:
             bind_template(child, name, node)
--- funkybomb/util.py
+++ funkybomb/util.py
@@ -3,23 +3,23 @@ from funkybomb.node import Template
 
 def generate(node, prefix, indent, context):
 
-    if isinstance(node, Template) and node._name and context and \
-            node._name in context:
-        node = context[node._name]
+    if isinstance(node, Template) and node._name_ and context and \
+            node._name_ in context:
+        node = context[node._name_]
 
-    if node._opener:
-        yield prefix + node._opener
+    if node._opener_:
+        yield prefix + node._opener_
 
     new_prefix = prefix
-    if node._opener and node._closer:
+    if node._opener_ and node._closer_:
         new_prefix += indent
 
     for child in node:
         for output in generate(child, new_prefix, indent, context):
             yield output
 
-    if node._closer:
-        yield prefix + node._closer
+    if node._closer_:
+        yield prefix + node._closer_
 
 
 def render(node, context=None, pretty=False):
@@ -30,6 +30,6 @@ def render(node, context=None, pretty=False):
 
 
 def freeze(node):
-    node._children = tuple(node._children)
+    node._children_ = tuple(node._children_)
     for child in node:
         freeze(child)
